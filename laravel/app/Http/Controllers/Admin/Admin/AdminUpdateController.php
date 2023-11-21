eb8d1eaaf03ab19b9fc57b028ea75712615ed194
Jannatul Ferdaous Lam <lam@itlogiko.com>
2023-11-21 16:37:49 +0600

    - deleted extra code

--- laravel/app/Http/Controllers/Admin/Admin/AdminUpdateController.php
+++ laravel/app/Http/Controllers/Admin/Admin/AdminUpdateController.php
@@ -70,78 +70,10 @@ class AdminUpdateController extends Controller
                         "gender" => $validated['gender'],
                     ]),
                 ],
-                'message' => 'Admin update successful.',
+                'message' => 'Admin update without image successful.',
             ]);
         }else{
             return 'image not found';
         }
-
-        // return response()->json([
-        //     'data' => [
-        //         'admin' => $admin->update([
-        //             "first_name" => $validated['first_name'],
-        //             "last_name" => $validated['last_name'],
-        //             "email" => $validated['email'],
-        //             "designation" => $validated['designation'],
-        //             "department" => $validated['department'],
-        //             "password" => $validated['password'],
-        //             "user_information" => $validated['user_information'],
-        //             "user_information" => $validated['user_information'],
-        //             "image" => $imageName,
-        //             "gender" => $validated['gender'],
-        //         ]),
-        //     ],
-        //     'message' => 'Admin update successful.',
-        // ]);
-
-
-
-        }
-
-
-
-    //     $admin->fill($request->post())->update();
-
-    //     if($request->has('image')){
-
-    //         if($admin->image){
-    //             $exists = Storage::disk('public')->exists(`AD_img/{$admin->image}`);
-    //             if($exists){
-    //                 Storage::disk('public')->delete(`AD_img/{$admin->image}`);
-    //             }
-    //         }
-
-    //         $image = $request->file('image');
-
-    //         $imageName = time().'.'.$image->getClientOriginalExtension();
-
-    //         $image->storeAs('public/AD_img', $imageName);
-
-    //         $admin->image = $imageName;
-
-    //         $admin->save();
-
-    //     }
-
-    // $admin->update($request->validated());
-
-    //     return response()->json([
-    //         'data' => [
-
-    //             $validated = $request->validated(),
-    //             'admin' => $admin->update([
-    //                 "first_name" => $validated['first_name'],
-    //                 "last_name" => $validated['last_name'],
-    //                 "email" => $validated['email'],
-    //                 "designation" => $validated['designation'],
-    //                 "department" => $validated['department'],
-    //                 "password" => $validated['password'],
-    //                 "user_information" => $validated['user_information'],
-    //                 "image" => $imageName,
-    //                 "gender" => $validated['gender'],
-    //             ]),
-    //         ],
-    //         'message' => 'Admin update successful.',
-    //     ]);
-
     }
+}
